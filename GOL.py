2144dc2e825d32c03998b88ec01504e305b5ce81
Oozna <johanssonericwilliam@gmail.com>
2022-02-03 11:52:29 +0100

    Cleaned up code *cough* Marcus *couch*

--- GOL.py
+++ GOL.py
@@ -1,11 +1,11 @@
 # Made by Eric Johansson
-# Date 15/1/2022
-# Version 1.0
+# Date 3/2/2022
+# Version 1.1
 import os
 import time
 
-
-
+# Checks OS and sets appropriate clear command
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
 # Creates a 20x20 board filled with " " with a border of "#"s and some "*"s in starting positions 
 def createBoard():
@@ -33,60 +33,47 @@ def printBoard(board):
 
 # checks all of the neighbors of a cell and returns the number of neighbors, including diagonals
 def countNeighbors(board, i, j):
-		count = 0
-		if board[i-1][j] == "*":
+	count = 0
+	for x in range(-1, 2):
+		for y in range(-1, 2):
+			if board[i+x][j+y] == "*":
 				count += 1
-		if board[i+1][j] == "*":
-				count += 1
-		if board[i][j-1] == "*":
-				count += 1
-		if board[i][j+1] == "*":
-				count += 1
-		if board[i-1][j-1] == "*":
-				count += 1
-		if board[i-1][j+1] == "*":
-				count += 1
-		if board[i+1][j-1] == "*":
-				count += 1
-		if board[i+1][j+1] == "*":
-				count += 1
-		return count
 
+	# To remove the cell itself from the count
+	if board[i][j] == "*":
+		count -= 1
+	return count
 
 # Checks if the cells should die, stay alive or be born in the next generation
 def updateBoard(board):
-		newBoard = createBoard()
-		for i in range(1, 21):
-				for j in range(1, 21):
-						if board[i][j] == "*":
-							if countNeighbors(board, i, j) < 2:
-								newBoard[i][j] = " "
-							elif countNeighbors(board, i, j) > 3:
-								newBoard[i][j] = " "
-							else:
-								newBoard[i][j] = "*"
-						elif board[i][j] == " ":
-							if countNeighbors(board, i, j) == 3:
-								newBoard[i][j] = "*"
-							else:
-								newBoard[i][j] = " "
-		return newBoard
+	newBoard = createBoard()
+	for i in range(1, 21):
+			for j in range(1, 21):
+					if board[i][j] == "*":
+						if countNeighbors(board, i, j) < 2:
+							newBoard[i][j] = " "
+						elif countNeighbors(board, i, j) > 3:
+							newBoard[i][j] = " "
+						else:
+							newBoard[i][j] = "*"
+					elif board[i][j] == " ":
+						if countNeighbors(board, i, j) == 3:
+							newBoard[i][j] = "*"
+						else:
+							newBoard[i][j] = " "
+	return newBoard
 
 # Main function
 def rungame():
 	generations = int(input("How many generations? "))
 	board = createBoard()
-	os.system('cls') # Clears the screen. Change to 'clear' if on mac
+	clearConsole()
 	printBoard(board)
-	time.sleep(0.5) # Pauses the game for 0.5 seconds
 
 	for i in range(generations-1):
-		os.system('cls') # Clears the screen. Change to 'clear' for mac
+		time.sleep(0.5) # Pauses the game for 0.5 seconds
+		clearConsole()
 		board = updateBoard(board)
 		printBoard(board)
-		time.sleep(0.5) # Pauses the game for 0.5 seconds
-
-
-
 
 rungame()
